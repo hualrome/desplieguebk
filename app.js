73fbfefaf264a139472876d9d29120f7b09237f3
Hugo Rojas <harojas538@soy.sena.edu.co>
2024-10-07 16:45:18 -0500

    backend3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({origin:'http://localhost:4200'}));
+app.use(cors({origin:'*'}));
 app.use('/api/usuarios',require ('./routes/usuario.routs'));
 app.use('/api/empleados',require ('./routes/empleado.routs'));
 app.listen(3005, ()=>{
